4024015af5318ee3b61cfaf3630074ff94d245ce
cemanaral <cemanaral425@gmail.com>
2021-03-31 20:04:23 +0300

    segmentation fault

--- main.c
+++ main.c
@@ -20,12 +20,14 @@ struct Customer {
     char* name;
     char* surname;
     struct Basket* basketList;
+    struct Customer* nextPtr;
 };
 
 struct Basket {
     int id;
     struct Product* productList;
     int amount; // Total price
+    struct Basket* nextPtr;
 };
 
 struct Product {
@@ -56,14 +58,11 @@ ProductPtr headProduct = NULL;
 // TODO: findName() 
 
 
-int addCustomer(int id, char* name, char* surname) {
+void addCustomer(int id, char* name, char* surname) {
     /*
         Adds customer to headCustomer linked list.
-        return 1: succesful
-        return 0  failed
     */
-    CustomerPtr newCustomerPtr = malloc(sizeof(Customer));
-    CustomerPtr previousCustomerPtr = NULL;
+    CustomerPtr newCustomerPtr = (CustomerPtr)malloc(sizeof(Customer));
     CustomerPtr currentCustomerPtr = headCustomer;
 
     // insert starts here
@@ -71,10 +70,39 @@ int addCustomer(int id, char* name, char* surname) {
     strcpy( newCustomerPtr->name, name );
     strcpy( newCustomerPtr->surname, surname );
     newCustomerPtr->basketList = NULL;
+    newCustomerPtr->nextPtr = NULL;
     ///////////
 
+    // If linked list is empty, inserts to head and terminates function
+    if (headCustomer == NULL) {
+        headCustomer = newCustomerPtr;
+        return;
+    }
 
-    return 1;
+
+    // Finds the last node
+    while (currentCustomerPtr->nextPtr != NULL) {
+        currentCustomerPtr = currentCustomerPtr->nextPtr;
+    }
+    
+    currentCustomerPtr->nextPtr = newCustomerPtr;
+}
+
+void printCustomers() {
+    CustomerPtr currentNode = headCustomer;
+
+    if (headCustomer == NULL) {
+        puts("Customer List is empty");
+        return;
+    }
+
+    puts("**Customer List**");
+    while (currentNode->nextPtr != NULL) {
+        printf("id: %d, name: %s, surname: %s\n", 
+            currentNode->id, currentNode->name, currentNode->surname);
+        currentNode = currentNode->nextPtr;
+    }
+    printf("\n");
 }
 
 void readCustomerFile() {
@@ -94,8 +122,8 @@ void readCustomerFile() {
     // Reading starts here
     while(fgets(currentLine, 100, file)) {
         sscanf(currentLine, "%d %s %s", &id, name, surname);
-
-        // printf("id: %d, name: %s, surname: %s\n", id, name, surname);
+        printf("%d", id);
+        addCustomer(id, name, surname);
     }
 
     fclose(file);
@@ -103,7 +131,7 @@ void readCustomerFile() {
 
 int main() {
     readCustomerFile();
-
+    // printCustomers();
 
     return 0;
 }
