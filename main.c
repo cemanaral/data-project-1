1f64a0351e7974879e5a61634ed5e519178b33d4
cemanaral <cemanaral425@gmail.com>
2021-03-31 18:23:36 +0300

    implemented node

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* nextPtr;
+};
+
+typedef struct Node Node;
+typedef Node* NodePtr;
+
+int main() {
+    return 0;
+}
+
